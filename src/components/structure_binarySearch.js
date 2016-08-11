ad809977b4cdd09331db174a6d0083b8d023ad98
Jenna Vuong <jennavuong@gmail.com>
2016-08-11 15:22:36 -0700

    remove constructure(props) not needed in structure_binarySearch.js

--- src/components/structure_binarySearch.js
+++ src/components/structure_binarySearch.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 
 export default class Structure_BinarySearch extends Component {
-
-	constructor(props) {
-		super(props)
-	}
+	
   render() {
       return (
       	<div>
